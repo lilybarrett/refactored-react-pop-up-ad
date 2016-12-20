8a099c51ec29fc4dd559f36922893eaf8e74851b
lilybarrett <lily@barrett.us>
2016-12-20 10:44:45 -0500

    Introduce bugs/common React gotchas

--- src/components/AdContainer.js
+++ src/components/AdContainer.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import CloseButton from './CloseButton';
 import EmailForm from './EmailForm';
-import PopUpText from './PopUpText';
 
 const AdContainer = props => {
   return (
     <div className="pink row">
       <div className="small-12 text-right columns">
-        <CloseButton handleButtonClick={props.handleButtonClick}/>
+        <CloseButton />
       </div>
       <div className="row">
         <div className="small-9 small-centered columns">
           <PopUpText />
-          <EmailForm handleButtonSubmit={props.handleButtonSubmit}/>
+          <EmailForm handleButtonSubmit={props.handlebuttonSubmit}/>
         </div>
       </div>
     </div>
--- src/components/PopUpText.js
+++ src/components/PopUpText.js
@@ -4,8 +4,8 @@ const PopUpText = props => {
   return (
     <div className="text-center pop-up-text">
       <h1>You have won tickets to see Taylor Swift!</h1>
-      <h3>Please enter your email so we can send you the tickets.</h3>
     </div>
+    <h3>Please enter your email so we can send you the tickets.</h3>
   );
 };
 
